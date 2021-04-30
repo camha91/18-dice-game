c47abe3b85a935ae8c755d2329b6ab6bd2d971de
camha91 <crystalhanguyen@gmail.com>
2021-04-30 16:41:22 -0700

    Updated UI

--- src/GourdCrabShrimpFish/DicesList.js
+++ src/GourdCrabShrimpFish/DicesList.js
@@ -5,28 +5,28 @@ export default function DicesList() {
     <div className="mt-5 p-3">
       <div
         className="bg-white ml-2"
-        style={{ width: 600, height: 600, borderRadius: 150 }}
+        style={{ width: 300, height: 300, borderRadius: 150 }}
       >
         <div className="row">
-          <div className="col-12 text-center  mt-5">
+          <div className="col-12 text-center mt-5">
             <img
-              style={{ width: 150 }}
+              style={{ width: 60 }}
               src="./img/GourdCrabShrimpFish/gourd.png"
               alt="gourd.png"
             />
           </div>
         </div>
-        <div className="row mt-5">
+        <div className="row mt-3">
           <div className="col-6 p-5 text-right">
             <img
-              style={{ width: 150 }}
+              style={{ width: 60 }}
               src="./img/GourdCrabShrimpFish/gourd.png"
               alt="gourd.png"
             />
           </div>
           <div className="col-6 p-5">
             <img
-              style={{ width: 150 }}
+              style={{ width: 60 }}
               src="./img/GourdCrabShrimpFish/gourd.png"
               alt="gourd.png"
             />
@@ -35,7 +35,7 @@ export default function DicesList() {
       </div>
       <div className="text-center mt-5">
         <button
-          style={{ fontSize: "50px", width: 300, borderRadius: "20px" }}
+          style={{ fontSize: "50px", width: 150, borderRadius: "20px" }}
           className="btn btn-primary"
         >
           Roll
