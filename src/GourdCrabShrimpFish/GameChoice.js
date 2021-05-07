6db282d3b0c5a66436e42754ea9ec6e33ec279bf
camha91 <crystalhanguyen@gmail.com>
2021-05-06 17:58:37 -0700

    Use animation useSpring for button increase, decrease

--- src/GourdCrabShrimpFish/GameChoice.js
+++ src/GourdCrabShrimpFish/GameChoice.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
+import { useSpring, animated } from "react-spring";
 
 export default function GameChoice(props) {
   const { gameChoice } = props;
 
   const dispatch = useDispatch();
+
+  const [decrease, setDecrease] = useState(false);
+
+  const propsUseSpringDecrease = useSpring({
+    from: { scale: 1 },
+    to: { scale: 1.25 },
+    reset: true,
+  });
+
+  const [increase, setIncrease] = useState(false);
+
+  const propsUseSpringIncrease = useSpring({
+    from: { scale: 1 },
+    to: { scale: 1.25 },
+    reset: true,
+  });
 
   return (
     <div className="mt-3">
@@ -14,33 +31,39 @@ export default function GameChoice(props) {
         alt={gameChoice.img}
       />
       <div className="bg-success mt-2 pb-2 text-center">
-        <button
-          onClick={() =>
+        <animated.button
+          style={propsUseSpringDecrease}
+          onClick={() => {
+            setDecrease({ scale: decrease ? 1.25 : 1 });
+
             dispatch({
               type: "BET_POINT",
               gameChoice,
               changePoint: false,
-            })
-          }
+            });
+          }}
           className="btn btn-danger mr-3"
         >
           <i className="fa fa-minus"></i>
-        </button>
+        </animated.button>
         <span style={{ fontSize: "25px", color: "yellow" }}>
           {gameChoice.point}
         </span>
-        <button
-          onClick={() =>
+        <animated.button
+          style={propsUseSpringIncrease}
+          onClick={() => {
+            setIncrease({ scale: increase ? 1.25 : 1 });
+
             dispatch({
               type: "BET_POINT",
               gameChoice,
               changePoint: true,
-            })
-          }
+            });
+          }}
           className="btn btn-danger ml-3"
         >
           <i className="fa fa-plus"></i>
-        </button>
+        </animated.button>
       </div>
     </div>
   );
